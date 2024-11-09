0757efa4fba2cfd2789b14e6acd00ff57a13d8a8
NguyenXuanVinh <nongami87@gmail.com>
2024-11-09 23:36:42 +0700

    style: improve code readability by breaking down long lines

--- src/main/java/app/libmgmt/model/Loan.java
+++ src/main/java/app/libmgmt/model/Loan.java
@@ -1,7 +1,5 @@
 package app.libmgmt.model;
 
-import app.libmgmt.dao.BookDAO;
-
 import java.util.Date;
 
 public class Loan {
@@ -11,9 +9,10 @@ public class Loan {
     private String isbn;
     private int userId;
     private String status;
-    private final long twoWeeksInMillis = 1209600000 ; // 14L * 24 * 60 * 60 * 1000
+    private final long twoWeeksInMillis = 1209600000; // 14L * 24 * 60 * 60 * 1000
 
-    public Loan(int loanId, Date borrowedDate, Date returnedDate, String isbn, int userId, String status) {
+    public Loan(int loanId, Date borrowedDate, Date returnedDate, String isbn, int userId,
+            String status) {
         this.loanId = loanId;
         this.borrowedDate = borrowedDate;
         this.returnedDate = returnedDate;
@@ -66,6 +65,10 @@ public class Loan {
 
     @Override
     public String toString() {
-        return "Loan ID: " + loanId + ", ISBN: " + isbn + ", User ID: " + userId + ", Borrowed Date: " + borrowedDate + ", Status: " + getStatus();
+        return "Loan ID: " + loanId
+                + ", ISBN: " + isbn
+                + ", User ID: " + userId
+                + ", Borrowed Date: " + borrowedDate
+                + ", Status: " + getStatus();
     }
 }
