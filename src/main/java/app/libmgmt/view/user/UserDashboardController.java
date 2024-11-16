afaace88d85f7f2337e9c3542874b9c0c5d79b3e
mrthinh307 <23020708@vnu.edu.vn>
2024-11-16 17:00:29 +0700

    feat(user): add Dashboard to the User interface

--- src/main/java/app/libmgmt/view/user/UserDashboardController.java
+++ src/main/java/app/libmgmt/view/user/UserDashboardController.java
@@ -1,4 +1,23 @@
 package app.libmgmt.view.user;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
 public class UserDashboardController {
+
+    @FXML
+    private PieChart pieChart;
+
+    @FXML
+    private Pane pieChartPane;
+
+    @FXML
+    private StackPane stackPaneContainer;
+
+    @FXML
+    private Text textQuote;
+
 }
